d5c58db0cac71fa7d17053b27a3c5dd9ef93d784
Patrick San Juan <psj.webdev@gmail.com>
2017-05-15 13:43:19 -0700

    jsonify imagesearch result

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #!image-abstraction/bin/python
 import requests
-from flask import Flask
+import json
+from flask import Flask, jsonify
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
@@ -10,10 +11,20 @@ GOOGLE_AUTH_KEY = "AIzaSyD4-UXkrmqT-nj4pqcbEMBPF13I8QFhRIU"
 GOOGLE_CUSTOM_SEARCH_URL = 'https://www.googleapis.com/customsearch/v1'
 GOOGLE_SEARCH_ENGINE = "016465477141158983016:zw10rls-o5c"
 
+def makeDict(item):
+	return {
+		'title': item['title'],
+		'html': item['htmlSnippet'],
+		'link': item['link'],
+		'thumb': item['image']['thumbnailLink']
+	}
+
 @app.route('/imagesearch/<query>')
 def image_search(query):
 	url = "{0}?key={1}&searchType=image&num=10&q={2}&cx={3}".format(GOOGLE_CUSTOM_SEARCH_URL, GOOGLE_AUTH_KEY, query, GOOGLE_SEARCH_ENGINE)
-	return requests.get(url).content
+	the_dict = json.loads(requests.get(url).text)
+	result = [makeDict(item) for item in the_dict['items']]
+	return jsonify(result)
 
 @app.route('/history')
 def history():
