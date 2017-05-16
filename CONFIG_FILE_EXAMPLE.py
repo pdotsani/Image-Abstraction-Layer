f0edcb5b0c922e946facbeed72422ad089aa8943
Patrick San Juan <psj.webdev@gmail.com>
2017-05-16 11:10:42 -0700

    example python config file

---
+++ CONFIG_FILE_EXAMPLE.py
@@ -0,0 +1,9 @@
+"""
+CONFIG FILE
+
+Make a copy of this file and name it CONFIG_FILE. Replace fields with
+registered keys on google console.
+
+"""
+GOOGLE_AUTH = "auth key here"
+GOOGLE_SEARCH_ENGINE = "search engine key here"
